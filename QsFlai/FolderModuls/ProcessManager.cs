fc87d48d96a83ad7a014a61c7b48b977011a143c
Den <you@example.com>
2020-10-10 20:48:09 +0300

    example

---
+++ QsFlai/FolderModuls/ProcessManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QsFlai.FolderModuls
+{
+    public class ProcessManager
+    {
+        ProcessStartInfo procStartInfo;
+        Process proc;
+
+        public ProcessManager(string fileName)
+        {
+            procStartInfo = new ProcessStartInfo("explorer", fileName);
+            proc = new Process();
+            proc.StartInfo = procStartInfo;
+        }
+
+        public void StartProcess()
+        {
+            proc.Start();
+        }
+    }
+}
---
+++ QsFlai/UserControls/Folder.xaml.cs
@@ -0,0 +1,51 @@
+﻿using QsFlai.FolderModuls;
+using QsFlai.Preferences;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using sys = System.IO;
+
+namespace QsFlai.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Folder.xaml
+    /// </summary>
+    public partial class Folder : UserControl
+    {
+        private ProcessManager process;
+        public Folder(File file)
+        {
+            InitializeComponent();
+
+            process = new ProcessManager(file.Link);
+
+            var img = new ImageLoader(ref logo);
+            img.LoadImage(file.Image);
+
+            name.Content = String.IsNullOrEmpty(file.Name) ? getFileName(file.Link) : file.Name;
+        }
+
+        private string getFileName(string path)// Вынести в отдельный модуль
+        {
+            return new sys.FileInfo(path).Name;
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Open file
+            process.StartProcess();
+        }
+    }
+}
